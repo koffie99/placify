1e79eecf22a5f856d7bf86e91a027c3caa51e3c1
Joshua <koffworld@gmail.com>
2023-07-11 08:05:28 +0000

    changes on navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,10 @@ import React, { useState, useEffect } from 'react'
 
 const Navbar = () => {
 
-  const [lightMode, setLightMode] = useState(false)
-  const [outline, setOutline] = useState(false)
+  const [lightMode, setLightMode] = useState(false);
+  const [outline, setOutline] = useState(false);
   const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const handleMode = () => {
     setLightMode((prevState) => !prevState);
@@ -45,14 +46,14 @@ const Navbar = () => {
     <div className='h-[60vh] p-11 text-white flex flex-col gap-10 justify-evenly' id='nav'>
         <div className='flex items-center justify-between'>
           <h1 className='font-bold'>Placify</h1>
-          <div onClick={handleMode} className='cursor-pointer'>
+          <div onClick={handleMode} className='cursor-pointer bg-[#183642] p-3 rounded-full'>
             {
               lightMode ? <DarkModeOutlined /> : <LightModeOutlined />
             }
           </div>
         </div>
         <div className='flex flex-col justify-center gap-5'>
-          <h1 className='font-bold text-4xl text-center'>Let's take a trip to {places.length} beautiful places today!</h1>
+          <h1 className='font-bold text-4xl text-center'>Let's take a trip to {loading ? '' : places.length} beautiful places today!</h1>
           <form action="" className='flex items-center justify-center'>
             <div className={`p-3 rounded-md bg-white flex items-center justify-between w-full md:w-[50%] ${outline ? 'bg-transparent ring-1 ring-white text-white' : 'bg-white'}`} onFocus={handleOutline}>
               <input type="text" placeholder='Search Pacify...' className={`outline-none w-[80%] md:w-[100%] ${outline ? 'bg-transparent' : 'bg-white'}`} onFocus={handleOutline}/>
